ca9f21dbad87754a081e888def29898c851ba3ae
Spikemax9 <Spikemax9@gmail.com>
2022-04-03 17:34:31 +0500

    add class CalcTime

---
+++ src/ru/progwards/java1/lessons/static1/CalcTime.java
@@ -0,0 +1,48 @@
+package ru.progwards.java1.lessons.static1;
+
+public class CalcTime {
+    static double distance;
+    static double speed;
+    static String cityFrom;
+    static String cityTo;
+
+    public static void printTime(){
+        double time = (distance * 1000 / speed * 3.6) / 3600;
+        System.out.printf("Если ехать из %s в %s со скоростью %.1f км/ч, то %.1f км проедем за %.1f часов.", cityFrom, cityTo, speed, distance, time);
+        System.out.println();
+    }
+
+    public static void MoscowToPiter(){
+        distance = 712;
+        speed = 80;
+        cityFrom = "Москва";
+        cityTo = "Питер";
+    }
+
+    public static void LissabonToVladivostok(){
+        distance = 13090;
+        speed = 75;
+        cityFrom = "Лиссабон";
+        cityTo = "Владивосток";
+    }
+
+    public static void MurmanskToAlmata(){
+        distance = 5413;
+        speed = 60;
+        cityFrom = "Мурманск";
+        cityTo = "Алмата";
+    }
+
+    public static void printTimes(){
+        MoscowToPiter();
+        printTime();
+        LissabonToVladivostok();
+        printTime();
+        MurmanskToAlmata();
+        printTime();
+    }
+
+    public static void main(String[] args) {
+        printTimes();
+    }
+}
