bb0db48783ca3f54b40fc645ea920535c3579072
Spikemax9 <Spikemax9@gmail.com>
2022-04-03 17:48:29 +0500

    add class Goodies

---
+++ src/ru/progwards/java1/lessons/static1/Goodies.java
@@ -0,0 +1,37 @@
+package ru.progwards.java1.lessons.static1;
+
+public class Goodies {
+    static String goodies;
+    static String child;
+
+    public static void printPresent(){
+        System.out.printf("%s дали %s", child, goodies);
+        System.out.println();
+    }
+    public static void masha(){
+        child = "Маше";
+    }
+    public static void cookie(){
+        goodies = "печеньку";
+    }
+    public static void setChild(String child){
+        Goodies.child = child;
+    }
+    public static void setGoodies(String goodies){
+        Goodies.goodies = goodies;
+    }
+    public static void printPresents(){
+        masha();
+        printPresent();
+        setGoodies("леденец");
+        printPresent();
+        cookie();
+        printPresent();
+        setChild("Пете");
+        printPresent();
+    }
+
+    public static void main(String[] args) {
+        printPresents();
+    }
+}
