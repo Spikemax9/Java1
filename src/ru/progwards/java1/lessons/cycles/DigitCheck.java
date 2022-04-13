f8b220056e0aabb43544924e26de283507b8513a
Spikemax9 <Spikemax9@gmail.com>
2022-04-13 21:26:35 +0500

    changed DigitCheck

--- src/ru/progwards/java1/lessons/cycles/DigitCheck.java
+++ src/ru/progwards/java1/lessons/cycles/DigitCheck.java
@@ -3,7 +3,7 @@ package ru.progwards.java1.lessons.cycles;
 public class DigitCheck {
     public static boolean containsDigit(int number, int digit){
         boolean flag = false;
-        for (int i = 0; i < number; i++) {
+        for (int i = 1; i < number; i++) {
             if(i == digit){
                 flag = true;
                 break;
@@ -13,6 +13,6 @@ public class DigitCheck {
     }
 
     public static void main(String[] args) {
-        System.out.println(containsDigit(5, 3));
+        System.out.println(containsDigit(0, 0));
     }
 }
