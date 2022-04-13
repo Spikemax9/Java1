c988ce9d4d79af4b8bf653084f62ffa5505f9815
Spikemax9 <Spikemax9@gmail.com>
2022-04-13 20:11:46 +0500

    changed sumEvenIdx

---
+++ src/ru/progwards/java1/lessons/cycles/NumbersRange.java
@@ -0,0 +1,25 @@
+package ru.progwards.java1.lessons.cycles;
+
+public class NumbersRange {
+    public static long sumNumbers(int start, int finish){
+        long result = 0;
+        for (int i = start; i <= finish; i++) {
+            result += i;
+        }
+        return result;
+    }
+    public static long sumOdd(int start, int finish){
+        long result = 0;
+        for (int i = start; i <= finish; i++) {
+            if(i % 2 == 0) result += i;
+        }
+        return result;
+    }
+    public static long sumEvenIdx(int start, int finish){
+        long result = 0;
+        for (int i = 1, j = start; j <= finish; i++, j++) {
+            if(i % 2 != 0) result += i;
+        }
+        return result;
+    }
+}
